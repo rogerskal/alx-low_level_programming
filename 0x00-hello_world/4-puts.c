02c492b7abe465947eddf7eb9e7ab351bb09e3c2
rogersakal <rogers.kalemera2@gmail.com>
2023-02-16 10:42:49 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  main - Write a C program that prints exactly "Programming is like building a multilingual puzzle
+ *  main - Programming is like building a multilingual puzzle
  *  Return: 0
  */
 int main(void)
