43c1b094c29a445e0176a89d5cdc64e063cbb577
rogersakal <rogers.kalemera2@gmail.com>
2023-03-11 12:59:10 -0800

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: arguments
+ * return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv; /*ingnore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
