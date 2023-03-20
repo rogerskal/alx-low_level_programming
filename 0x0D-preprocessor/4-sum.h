931eb87c4f86969b8decc97045571f920129bd39
rogersakal <rogers.kalemera2@gmail.com>
2023-03-20 10:20:04 -0700

    task4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,11 @@
-#ifndef SUM_H
-#define SUM_H
-
-#define SUM(x, y) ((x) + (y))
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+/**
+ * File: 4-sum
+ * @x: input
+ * @y: input
+ * Desc: header file
+ */
+#define SUM(x, y) (x + y)
 
 #endif
